13e0a36a787d50ae4e017c47860c4ee9b38aac01
Binod Raj pant <callme.binod07@gmail.com>
2025-09-27 14:42:52 +0545

    updated on 27th sep 2025 saturday

--- nss/nss/settings.py
+++ nss/nss/settings.py
@@ -60,7 +60,7 @@ INSTALLED_APPS = [
 ]
 
 AUTHENTICATION_BACKENDS = [
-    # 'django.contrib.auth.backends.ModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
